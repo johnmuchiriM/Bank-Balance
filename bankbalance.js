f3d98a401c4e7597fac141174518b7b1e87b28d9
John Muchiri <60730009+johnmuchiriM@users.noreply.github.com>
2021-03-16 12:15:43 +0300

    Create bankbalance.js

---
+++ bankbalance.js
@@ -0,0 +1,11 @@
+var bank_balance = prompt('Enter Bank Balance');
+var guitar = 34000;
+
+if (guitar < bank_balance){
+
+    console.log('You Bought the guitar');
+
+    }
+else{
+    console.log('Youre Almost there, Work Harder');
+    }
